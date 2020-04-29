21c3298dc465357453c41805036d8b659e964a72
Martin Emil Wøbbe <54886017+daiperboevs42@users.noreply.github.com>
2020-04-29 04:32:22 +0200

    UserDAO and User BE
    Created User BE and UserDAO with getUser method

---
+++ src/trackme/dal/UserDAO.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trackme.dal;
+
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import trackme.be.User;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author mac
+ */
+public class UserDAO {
+    
+    private final DBConnectionProvider connection;
+    
+    public UserDAO(){
+        connection = new DBConnectionProvider();
+    }
+    
+    public User getUser(String email, String password) throws SQLException{
+        String sql = "SELECT * FROM [User] WHERE email = ? AND password = ?";
+        
+        try(Connection con = connection.getConnection()){
+        PreparedStatement pstmt = con.prepareStatement(sql);
+        pstmt.setString(1, email);
+        pstmt.setString(2, password);
+        
+        ResultSet rs = pstmt.executeQuery();
+        rs.next();
+        int id = rs.getInt("id");
+        String name = rs.getString("name");
+        Integer type = rs.getInt("isAdmin");
+        
+        if(type.equals(1)){
+            return new User(id, name, User.UserType.ADMIN);
+        }
+        else {
+            return new User(id, name, User.UserType.EMPLOYEE);
+        }
+        
+        } catch (SQLServerException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        return null;
+    }
+    
+}
