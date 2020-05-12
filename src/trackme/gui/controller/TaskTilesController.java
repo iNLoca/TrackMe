ea1f2ff21e4c1556a03cc6d5edf16323ad18090a
Nadia Miteva <nadq.nadia@gmail.com>
2020-05-12 03:40:54 +0200

    css

--- src/trackme/gui/controller/TaskTilesController.java
+++ src/trackme/gui/controller/TaskTilesController.java
@@ -5,11 +5,24 @@
  */
 package trackme.gui.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import trackme.be.Project;
+import trackme.be.Task;
+import trackme.be.User;
+import trackme.bll.BLLManager;
 
 /**
  * FXML Controller class
@@ -24,7 +37,18 @@ public class TaskTilesController implements Initializable {
     private Label deslbl;
     @FXML
     private Label moneylbl;
+    @FXML
+    private JFXButton startbtn;
 
+    
+    
+     private ScheduledExecutorService absenceThreadExecutor;
+     private BLLManager bllManager;
+     private User user;
+    private Project project;
+    private Task task;
+    
+    private Label timecountlbl; ///Wrong 
     /**
      * Initializes the controller class.
      */
@@ -32,7 +56,42 @@ public class TaskTilesController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         //TODO
         
-     //   tasknamelbl.setText(tasktableview.getSelectionModel().getSelectedItem().getName());
+       
     }    
-    
+private boolean LOl = false;
+
+
+    @FXML
+    private void setStart(ActionEvent event) {
+        
+         if (startbtn != null && !LOl) {
+            LOl = true;
+            long startTime = System.currentTimeMillis();
+            absenceThreadExecutor = Executors.newSingleThreadScheduledExecutor();
+            absenceThreadExecutor.scheduleAtFixedRate(() -> {
+                Platform.runLater(() -> {
+
+                    long elapsedTime = System.currentTimeMillis() - startTime;
+                    long elapsedSeconds = (elapsedTime / 1000) % 60;
+                    long elapsedMinutes = ((elapsedTime / 1000) / 60) % 60;
+                    long elapsedHours = (((elapsedTime / 1000) / 60) / 60) % 24;
+
+                 timecountlbl.setText(elapsedHours + " : " + elapsedMinutes + " : " + elapsedSeconds);
+                });
+            }, 1, 1, TimeUnit.SECONDS);
+
+                     try {
+                         bllManager.insertTimeLog(user,project ,task , 0);
+                     } catch (SQLServerException ex) {
+                         Logger.getLogger(UserMainPageController.class.getName()).log(Level.SEVERE, null, ex);
+                     }
+         }
+    }
 }
+
+   
+             
+    
+
+    
+
