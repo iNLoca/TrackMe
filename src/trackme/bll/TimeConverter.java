a642a8ab6d01ea2515cae3b33ebcdf4bf907dfcb
Louise <mega_lulu_kryster@hotmail.com>
2020-05-30 02:25:05 +0200

    usermainpage imageview, bug fixing on timeconverter and timelogger

--- src/trackme/bll/TimeConverter.java
+++ src/trackme/bll/TimeConverter.java
@@ -20,72 +20,73 @@ import trackme.be.Project;
 import trackme.be.Task;
 import trackme.be.TimeLog;
 import trackme.dal.DALManager;
+
 /**
  *
  * @author WøbbePC
  */
 public class TimeConverter {
+
     private DALManager dalManager;
 
     public TimeConverter() {
         dalManager = new DALManager();
     }
-    
-    
-    
-    public void getProjectTime(List<Project> projects){
+
+    public void getProjectTime(List<Project> projects) {
 
         getTotalTimeForEachProject(projects);
-        
-          for (Project project : projects) {
-              System.out.println("Project :" +project.getName() + " Lol " + project.getTotalTimeInSeconds());
+
+        for (Project project : projects) {
+            System.out.println("Project :" + project.getName() + " Lol " + project.getTotalTimeInSeconds());
         }
 //        find time spent on task in one update?
     }
-    
-    public void getTotalTimeForTask(Task task) throws SQLServerException{
+
+    public void getTotalTimeForTask(Task task) throws SQLServerException {
         List<TimeLog> variable1 = task.getTaskTime();
-          long variable2 = 0;
-          long variableToDelete = 0;
-          long variableToDelete2 = 0; 
-          ZoneId zoneId = ZoneId.systemDefault(); 
-            for (int i = 0; i < variable1.size(); i++) {
-              if(i%2 == 1){
-                 variableToDelete = variable1.get(i).getTime().atZone(zoneId).toEpochSecond();
-                 variable2 += variableToDelete - variableToDelete2;
-              }else{
-                  variableToDelete2 = variable1.get(i).getTime().atZone(zoneId).toEpochSecond();
-              }
+        long variable2 = 0;
+        long variableToDelete = 0;
+        long variableToDelete2 = 0;
+        ZoneId zoneId = ZoneId.systemDefault();
+        for (int i = 0; i < variable1.size(); i++) {
+            if (i % 2 == 1) {
+                variableToDelete = variable1.get(i).getTime().atZone(zoneId).toEpochSecond();
+                variable2 += variableToDelete - variableToDelete2;
+            } else {
+                variableToDelete2 = variable1.get(i).getTime().atZone(zoneId).toEpochSecond();
             }
-            task.setTotalTimeInSeconds(variable2);
+        }
+        task.setTotalTimeInSeconds(variable2);
     }
-    
-    public void  getTotalTimeForEachProject(List<Project> projects){
+
+    public void getTotalTimeForEachProject(List<Project> projects) {
         for (Project project : projects) {
-          List<TimeLog> variable1 =  project.getProjectTime();
-          long variable2 = 0;
-          long variableToDelete = 0;
-          long variableToDelete2 = 0; 
-          ZoneId zoneId = ZoneId.systemDefault(); 
+            List<TimeLog> variable1 = project.getProjectTime();
+            long variable2 = 0;
+            long variableToDelete = 0;
+            long variableToDelete2 = 0;
+            ZoneId zoneId = ZoneId.systemDefault();
             for (int i = 0; i < variable1.size(); i++) {
-              if(i%2 == 1){
-                 variableToDelete = variable1.get(i).getTime().atZone(zoneId).toEpochSecond();
-                 variable2 += variableToDelete - variableToDelete2;
-              }else{
-                  variableToDelete2 = variable1.get(i).getTime().atZone(zoneId).toEpochSecond();
-              }
+                if (i % 2 == 1) {
+                    variableToDelete = variable1.get(i).getTime().atZone(zoneId).toEpochSecond();
+                    variable2 += variableToDelete - variableToDelete2;
+                } else {
+                    variableToDelete2 = variable1.get(i).getTime().atZone(zoneId).toEpochSecond();
+                }
             }
 
             project.setTotalTimeInSeconds(variable2);
         }
     }
-    public List<LocalDateTime> calculateTime(LocalTime startTime, LocalTime endTime, LocalDate date){
+
+    public List<LocalDateTime> calculateTime(LocalTime startTime, LocalTime endTime, LocalDate date) {
         List<LocalDateTime> newTimes = new ArrayList();
         Calendar cal = Calendar.getInstance(TimeZone.getTimeZone(ZoneId.systemDefault()));
         Date tempDate = Date.from(date.atStartOfDay(ZoneId.systemDefault()).toInstant());
         cal.setTime(tempDate);
         int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH)+1;
+        int month = cal.get(Calendar.MONTH) + 1;
         int day = cal.get(Calendar.DAY_OF_MONTH);
         Instant instant1 = startTime.atDate(LocalDate.of(year, month, day)).atZone(ZoneId.systemDefault()).toInstant();
         Instant instant2 = endTime.atDate(LocalDate.of(year, month, day)).atZone(ZoneId.systemDefault()).toInstant();
@@ -93,9 +94,10 @@ public class TimeConverter {
         LocalDateTime ldt2 = LocalDateTime.ofInstant(instant2, ZoneId.systemDefault());
         newTimes.add(ldt1);
         newTimes.add(ldt2);
-    return newTimes;
+        return newTimes;
+        
     }
-    
+
     public String convertSecondsToHourMinuteSecond(Task task) {
         String time = "";
         int p1 = (int) task.getTotalTimeInSeconds() / 3600;
